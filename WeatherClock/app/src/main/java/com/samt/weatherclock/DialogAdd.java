63eb0d9f53d64dc5efa496051597db3f18ccfdd7
zahariaca <zaharia.c.alexandru@gmail.com>
2016-12-06 15:00:36 +0200

    modified app toolbar and added a dailog xml and class

---
+++ WeatherClock/app/src/main/java/com/samt/weatherclock/DialogAdd.java
@@ -0,0 +1,23 @@
+package com.samt.weatherclock;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by AZaharia on 12/6/2016.
+ */
+
+public class DialogAdd extends DialogFragment {
+
+    public DialogAdd(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add, container, false);
+    }
+}
