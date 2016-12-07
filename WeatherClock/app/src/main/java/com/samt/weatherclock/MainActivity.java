c74c0c41167897ce1693afa67d92f208839f1b33
zahariaca <zaharia.c.alexandru@gmail.com>
2016-12-07 10:40:41 +0200

    added more data to be shown in the cards

--- WeatherClock/app/src/main/java/com/samt/weatherclock/MainActivity.java
+++ WeatherClock/app/src/main/java/com/samt/weatherclock/MainActivity.java
@@ -76,5 +76,14 @@ public class MainActivity extends AppCompatActivity {
         persons.add(new Person("Emma Wilson", "23 years old"));
         persons.add(new Person("Lavery Maiss", "25 years old"));
         persons.add(new Person("Lillie Watts", "35 years old"));
+        persons.add(new Person("Emma Wilson", "23 years old"));
+        persons.add(new Person("Lavery Maiss", "25 years old"));
+        persons.add(new Person("Lillie Watts", "35 years old"));
+        persons.add(new Person("Emma Wilson", "23 years old"));
+        persons.add(new Person("Lavery Maiss", "25 years old"));
+        persons.add(new Person("Lillie Watts", "35 years old"));
+        persons.add(new Person("Emma Wilson", "23 years old"));
+        persons.add(new Person("Lavery Maiss", "25 years old"));
+        persons.add(new Person("Lillie Watts", "35 years old"));
     }
 }
