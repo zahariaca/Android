615c9bf3c011306462b65de3332b0f9c107415af
zahariaca <zaharia.c.alexandru@gmail.com>
2016-12-13 12:09:25 +0200

    finishing touches for handling weather, created a nice dailog for choosing the city

--- WeatherClock/app/src/main/java/com/samt/weatherclock/fragments/DialogChangeLocation.java
+++ WeatherClock/app/src/main/java/com/samt/weatherclock/fragments/DialogChangeLocation.java
@@ -1,7 +1,11 @@
 package com.samt.weatherclock.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
@@ -21,6 +25,7 @@ public class DialogChangeLocation extends DialogFragment {
     private EditText editText;
     private ImageButton imageButtonClose;
     private Button buttonAdd;
+/*
 
     private View.OnClickListener buttonOnClickListener = new View.OnClickListener() {
 
@@ -36,20 +41,52 @@ public class DialogChangeLocation extends DialogFragment {
             dismiss();
         }
     };
+*/
 
     public DialogChangeLocation() {
     }
 
-    private void changeLocationInSharedPref() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        ((AlertDialog) getDialog()).getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+        ((AlertDialog) getDialog()).getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+
+
+        builder.setView(inflater.inflate(R.layout.dialog_change_location,null))
+                .setTitle("Input your location")
+                .setPositiveButton("Commit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        editText = (EditText) DialogChangeLocation.this.getDialog().findViewById(R.id.et_name);
+                        changeLocationInSharedPref(String.valueOf(editText.getText()));
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        DialogChangeLocation.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+
+    private void changeLocationInSharedPref(String editText) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        Log.d(LOG_TAG, "Changing current city with: " + String.valueOf(editText.getText()));
-        editor.putString("CityName", String.valueOf(editText.getText()));
+        Log.d(LOG_TAG, "Changing current city with: " + editText);
+        editor.putString("CityName", editText);
         editor.commit();
         Toast.makeText(getActivity().getApplicationContext(), "Please refresh weather page for the update to take place", Toast.LENGTH_SHORT).show();
     }
 
-
+/*
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -63,5 +100,5 @@ public class DialogChangeLocation extends DialogFragment {
         buttonAdd.setOnClickListener(buttonOnClickListener);
 
         return rootView;
-    }
+    }*/
 }
--- WeatherClock/app/src/main/java/com/samt/weatherclock/util/FetchWeatherTask.java
+++ WeatherClock/app/src/main/java/com/samt/weatherclock/util/FetchWeatherTask.java
@@ -39,28 +39,6 @@ public class FetchWeatherTask extends AsyncTask<String, Void, HashMap<String,Str
         this.finishedListener = finishedListener;
     }
 
-    /* The date/time conversion code is going to be moved outside the asynctask later,
-* so for convenience we're breaking it out into its own method now.
-*/
-    private String getReadableDateString(long time) {
-        // Because the API returns a unix timestamp (measured in seconds),
-        // it must be converted to milliseconds in order to be converted to valid date.
-        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-        return shortenedDateFormat.format(time);
-    }
-
-    /**
-     * Prepare the weather high/lows for presentation.
-     */
-    private String formatHighLows(double high, double low) {
-        // For presentation, assume the user doesn't care about tenths of a degree.
-        long roundedHigh = Math.round(high);
-        long roundedLow = Math.round(low);
-
-        String highLowStr = roundedLow + "/" + roundedHigh;
-        return highLowStr;
-    }
-
     /**
      * Take the String representing the complete forecast in JSON Format and
      * pull out the data we need to construct the Strings needed for the wireframes.
@@ -72,30 +50,35 @@ public class FetchWeatherTask extends AsyncTask<String, Void, HashMap<String,Str
             throws JSONException {
 
         // These are the names of the JSON objects that need to be extracted.
-        final String OWM_LIST = "list";
         final String OWM_WEATHER = "weather";
+        final String OWM_MAIN = "main";
+        final String OWM_SYS = "sys";
+        final String OWM_COUNTRY = "country";
+        final String OWM_NAME = "name";
+        final String OWM_DESCRIPTION = "description";
+        final String OWM_ID = "id";
+        final String OWM_SUNRISE = "sunrise";
+        final String OWM_SUNSET = "sunset";
         final String OWM_TEMPERATURE = "temp";
-        final String OWM_MAX = "max";
-        final String OWM_MIN = "min";
-        final String OWM_DESCRIPTION = "main";
+        final String OWM_HUMIDITY = "humidity";
+        final String OWM_PRESSURE = "pressure";
 
         HashMap<String, String> hashMap = new HashMap<String, String>();
 
         JSONObject forecastJson = new JSONObject(forecastJsonStr);
-        JSONObject weather = forecastJson.getJSONArray("weather").getJSONObject(0);
-        JSONObject main = forecastJson.getJSONObject("main");
-        JSONObject sys = forecastJson.getJSONObject("sys");
-        //JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+        JSONObject weather = forecastJson.getJSONArray(OWM_WEATHER).getJSONObject(0);
+        JSONObject main = forecastJson.getJSONObject(OWM_MAIN);
+        JSONObject sys = forecastJson.getJSONObject(OWM_SYS);
 
-        String country = sys.getString("country");
-        String cityName = forecastJson.getString("name");
-        String description = weather.getString("description");
-        String id = weather.getString("id");
-        String sunrise = sys.getString("sunrise");
-        String sunset = sys.getString("sunset");
-        String temperature = main.getString("temp");
-        String humidity = main.getString("humidity");
-        String pressure = main.getString("pressure");
+        String country = sys.getString(OWM_COUNTRY);
+        String cityName = forecastJson.getString(OWM_NAME);
+        String description = weather.getString(OWM_DESCRIPTION);
+        String id = weather.getString(OWM_ID);
+        String sunrise = sys.getString(OWM_SUNRISE);
+        String sunset = sys.getString(OWM_SUNSET);
+        String temperature = main.getString(OWM_TEMPERATURE);
+        String humidity = main.getString(OWM_HUMIDITY);
+        String pressure = main.getString(OWM_PRESSURE);
 
 
         DateFormat df = DateFormat.getDateTimeInstance();
@@ -111,67 +94,6 @@ public class FetchWeatherTask extends AsyncTask<String, Void, HashMap<String,Str
         hashMap.put("Humidity", "Humidity: " + humidity + "%");
         hashMap.put("Pressure", "Pressure: " + pressure + "hPa");
 
-
-
-
-
-
-        // OWM returns daily forecasts based upon the local time of the city that is being
-        // asked for, which means that we need to know the GMT offset to translate this data
-        // properly.
-
-        // Since this data is also sent in-order and the first day is always the
-        // current day, we're going to take advantage of that to get a nice
-        // normalized UTC date for all of our weather.
-/*
-        Time dayTime = new Time();
-        dayTime.setToNow();
-
-        // we start at the day returned by local time. Otherwise this is a mess.
-        int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-
-        // now we work exclusively in UTC
-        dayTime = new Time();
-
-        //String[] resultStrs = new String[numDays];
-        for (int i = 0; i < weatherArray.length(); i++) {
-            // For now, using the format "Day, description, hi/low"
-            String day;
-            String description;
-            String highAndLow;
-
-            // Get the JSON object representing the day
-            JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-            // The date/time is returned as a long.  We need to convert that
-            // into something human-readable, since most people won't read "1400356800" as
-            // "this saturday".
-            long dateTime;
-            // Cheating to convert this to UTC time, which is what we want anyhow
-            dateTime = dayTime.setJulianDay(julianStartDay + i);
-            day = getReadableDateString(dateTime);
-
-            // description is in a child array called "weather", which is 1 element long.
-            JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-            description = weatherObject.getString(OWM_DESCRIPTION);
-
-            // Temperatures are in a child object called "temp".  Try not to name variables
-            // "temp" when working with temperature.  It confuses everybody.
-            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-            double high = temperatureObject.getDouble(OWM_MAX);
-            double low = temperatureObject.getDouble(OWM_MIN);
-
-            //String cityName = dayForecast.getString("name");
-
-            highAndLow = formatHighLows(high, low);
-            //resultStrs[i] = day + " - " + description + " - " + highAndLow;*/
-
-/*            hashMap.put("Day", day);
-            hashMap.put("Description", description);
-            hashMap.put("Temperature", highAndLow);*/
-
-
-        //}
         return hashMap;
     }
 
